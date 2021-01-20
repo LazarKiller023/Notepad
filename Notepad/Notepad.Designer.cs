b748ffdf1fbf1e76fd1e891c554c329b17e139c1
LazarKiller023 <amstaf.nina555@gmail.com>
2021-01-20 15:05:14 +0100

    Commit 2

--- Notepad/Notepad.Designer.cs
+++ Notepad/Notepad.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Notepad
 {
-    partial class Form1
+    partial class Notepad
     {
         /// <summary>
         /// Required designer variable.
@@ -33,16 +33,19 @@ namespace Notepad
             this.menuStrip = new System.Windows.Forms.MenuStrip();
             this.filesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox
             // 
             this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Font = new System.Drawing.Font("Bahnschrift", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox.Location = new System.Drawing.Point(0, 24);
             this.textBox.Multiline = true;
             this.textBox.Name = "textBox";
@@ -78,31 +81,46 @@ namespace Notepad
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
             this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.newToolStripMenuItem.Text = "New";
-            // 
-            // saveToolStripMenuItem
-            // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.saveToolStripMenuItem.Text = "Save";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
-            // quitToolStripMenuItem
+            // saveToolStripMenuItem
             // 
-            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
-            this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.quitToolStripMenuItem.Text = "Quit";
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
             this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
             // 
-            // Form1
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "Text documents|*.txt";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Text documents|*.txt";
+            this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Notepad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -110,8 +128,10 @@ namespace Notepad
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Notepad";
+            this.Text = "Notepad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Notepad_FormClosing);
+            this.Load += new System.EventHandler(this.Notepad_Load);
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
@@ -129,6 +149,8 @@ namespace Notepad
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
 
---
+++ Notepad/Notepad.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class Notepad : Form
+    {
+        private string fileName = null;
+        private bool isUnsaved = false;
+        private bool ignoreTextChangedEvent = false;
+
+        public Notepad()
+        {
+            InitializeComponent();
+            UpdateTitle();
+        }
+
+        private void UpdateTitle()
+        {
+            string file;
+
+            if (string.IsNullOrEmpty(fileName))
+                file = "Unnamed";
+            else
+                file = fileName;
+
+            if (isUnsaved)
+                Text = file + "* - Notepad";
+            else
+                Text = file + " - Notepad";
+        }
+
+        private void SaveFile()
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    fileName = saveFileDialog.FileName;
+                else
+                    return;
+            }
+
+
+            File.WriteAllText(fileName, textBox.Text);
+            isUnsaved = false;
+            UpdateTitle();
+        }
+
+        private void filesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (ignoreTextChangedEvent)
+            {
+                ignoreTextChangedEvent = false;
+                return;
+            }
+            isUnsaved = true;
+            UpdateTitle();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            var eventArgs = new FormClosingEventArgs(CloseReason.None, false);
+            Notepad_FormClosing(null, eventArgs);
+
+            if (eventArgs.Cancel)
+                return;
+            
+            textBox.Text = string.Empty;
+            fileName = null;
+            isUnsaved = false;
+            UpdateTitle();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var eventArgs = new FormClosingEventArgs(CloseReason.None, false);
+            Notepad_FormClosing(null, eventArgs);
+
+            if (eventArgs.Cancel)
+                return;
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ignoreTextChangedEvent = true; 
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+                fileName = openFileDialog.FileName;
+                isUnsaved = false;
+                UpdateTitle();
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Notepad_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void Notepad_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(isUnsaved)
+            {
+                var res = MessageBox.Show(this, "Would you like to save changes?" , "Notepad", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+
+                if (res == System.Windows.Forms.DialogResult.Yes) 
+                {
+                    SaveFile();
+                }
+                else if(res == System.Windows.Forms.DialogResult.No)
+                {
+                    // Do nothing
+                }
+                else if(res == System.Windows.Forms.DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
